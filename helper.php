288856d8db1c687bbcbb7d5ea16baad6a298404b
Ali Güçlü <aliguclutr@gmail.com>
2021-05-31 00:50:09 +0300

    Update helper.php

--- helper.php
+++ helper.php
@@ -100,6 +100,30 @@ function base_url(string $url=null, bool $atRoot=false, bool $atCore=false, bool
 }
 
 /**
+ * @param  array  $parsed_url
+ * @param  string $type
+ * @return string
+ */
+function unparse_url(array $parsed_url=[], string $type='domain'): string
+{
+  $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+  $host     = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+  $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+  $user     = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+  $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
+  $pass     = ($user || $pass) ? "$pass@" : '';
+  $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+  $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+  $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+
+  if ($type == 'domain') {
+  	return "$scheme$user$pass$host$port";
+  } elseif ($type == 'path') {
+  	return "$scheme$user$pass$host$port$path$query$fragment";
+  }
+}
+
+/**
  * @return string|null
  */
 function page_url()
