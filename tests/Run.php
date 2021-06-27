db0c0a2d5a8159dc8f9b81fdea256be0cc0650a6
Ali Güçlü <aliguclutr@gmail.com>
2021-06-27 15:08:20 +0300

    ...

--- tests/Run.php
+++ tests/Run.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use BMVC\Core\{App, Route};
 use BMVC\Libs\MError;
--- tests/test.php
+++ tests/test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use BMVC\Core\App;
 use BMVC\Core\Route;
