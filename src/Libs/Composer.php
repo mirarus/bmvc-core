217c884ec2a032418b331a9546d6d51cb734192f
Ali Güçlü <aliguclutr@gmail.com>
2021-06-28 19:55:29 +0300

    ...

--- src/Libs/Composer.php
+++ src/Libs/Composer.php
@@ -8,30 +8,15 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-core
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.1
+ * @version 0.2
  */
 
 namespace BMVC\Libs;
 
-use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
-
 class Composer
 {
 	
-	/**
-	 * @param Event $event
-	 */
-	public static function postInstall(Event $event)
-	{
-		Dir::rm_dir(".git");
-		Dir::rm_dir(".github");
-	}
-
-	/**
-	 * @param Event $event
-	 */
-	public static function postUpdate(Event $event)
+	public static function folderDelete()
 	{
 		Dir::rm_dir(".git");
 		Dir::rm_dir(".github");
