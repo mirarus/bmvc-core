a68d30719ba33bde1241f33ca1c9b3c03170bc9b
Ali Güçlü <aliguclutr@gmail.com>
2021-08-02 18:46:45 +0300

    Update ClassException.php

--- src/Exception/ClassException.php
+++ src/Exception/ClassException.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.1
+ * @version 0.2
  */
 
 namespace BMVC\Exception;
@@ -19,13 +19,19 @@ use BMVC\Libs\{CL, FS};
 class ClassException extends Exception
 {
 
+	private static $namespaces = [
+		'BMVC\\Core', 
+		'BMVC\\Libs', 
+		'BMVC\\Exception'
+	];
+
 	public function __construct($message)
 	{
 		$class = CL::replace(str_replace([FS::app(), '.php'], null, self::getFile()));
 		
 		if (class_exists($class, false)) {
 		
-			$class = CL::replace(str_replace(['BMVC\\Core', 'BMVC\\Libs'], null, $class));
+			$class = CL::replace(str_replace(self::$namespaces, null, $class));
 			$message = '(' . $class . ') Error! | ' . $message;
 		}
 
