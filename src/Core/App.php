4626c6a955dc75969600e4e26359dfa0e628bd12
Ali Güçlü <aliguclutr@gmail.com>
2022-05-08 14:12:45 +0300

    9.2

--- src/Core/App.php
+++ src/Core/App.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-core
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 9.1
+ * @version 9.2
  */
 
 namespace BMVC\Core;
