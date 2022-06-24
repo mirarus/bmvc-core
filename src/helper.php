b06e49912ca36440a30b64d2afc182ee638c336e
Ali Güçlü <aliguclutr@gmail.com>
2022-06-24 22:14:17 +0300

    ...

--- src/helper.php
+++ src/helper.php
@@ -1,80 +1,10 @@
 <?php
 
-use BMVC\Libs\{FS, Route, Util};
+use BMVC\Libs\FS;
 
 FS::setPath(__DIR__);
-
-/**
- * @return null|string
- */
-function url(): ?string
-{
-  return Util::url(...func_get_args());
-}
-
-/**
- * @return null|string
- */
-function page(): ?string
-{
-  return Util::page_url();
-}
-
-/**
- * @return void
- */
-function pr()
-{
-  return Util::pr(...func_get_args());
-}
-
-/**
- * @return void
- */
-function dump()
-{
-  return Util::dump(...func_get_args());
-}
-
-/**
- * @return void
- */
-function redirect()
-{
-  return Util::redirect(...func_get_args());
-}
-
-/**
- * @return void
- */
-function refresh()
-{
-  return Util::refresh(...func_get_args());
-}
-
-function getErrors()
-{
-  return Route::getErrors(...func_get_args());
-}
-
-function getViewData()
-{
-  return \BMVC\Core\View::getData(...func_get_args());
-}
-
-function getViewContent()
-{
-  return \BMVC\Core\View::getContent();
-}
 
 function locales()
 {
   return \BMVC\Core\App::locales(...func_get_args());
-}
-
-if (!function_exists('_')) {
-  function _()
-  {
-    return func_get_arg(0);
-  }
 }
