a1d0b696eb8abbf286283c22cef91f7f30894f30
Ali Güçlü <aliguclutr@gmail.com>
2023-03-26 02:40:32 +0300

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -8,3 +8,18 @@ function locales()
 {
   return \BMVC\Core\App::locales(...func_get_args());
 }
+
+function iController()
+{
+  return \BMVC\Core\Controller::import(...func_get_args());
+}
+
+function iMiddleware()
+{
+  return \BMVC\Core\Middleware::import(...func_get_args());
+}
+
+function iModel()
+{
+  return \BMVC\Core\Model::import(...func_get_args());
+}
