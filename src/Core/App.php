7c93ec46d9b6657d4746ed2af7a3abe6ef2e5648
Ali Güçlü <aliguclutr@gmail.com>
2023-08-29 13:06:56 +0300

    Update App.php

--- src/Core/App.php
+++ src/Core/App.php
@@ -8,389 +8,407 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-core
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 9.29
+ * @version 9.30
  */
 
 namespace BMVC\Core;
 
-use BMVC\Libs\Model;
-use BMVC\Libs\FS;
 use BMVC\Libs\CL;
-use BMVC\Libs\Whoops;
 use BMVC\Libs\Env;
+use BMVC\Libs\Header;
+use BMVC\Libs\Locale;
+use BMVC\Libs\Model;
 use BMVC\Libs\Monolog;
 use BMVC\Libs\Request;
-use BMVC\Libs\Header;
 use BMVC\Libs\Route;
 use BMVC\Libs\Util;
-use BMVC\Libs\Locale;
+use BMVC\Libs\Whoops;
 
 final class App
 {
 
-  /**
-   * @var bool
-   */
-  private static $active = false;
+	/**
+	 * @var bool
+	 */
+	private static $active = false;
 
-  /**
-   * @var
-   */
-  public static $dotenv;
+	/**
+	 * @var
+	 */
+	public static $dotenv;
 
-  /**
-   * @var
-   */
-  public static $_microtime;
+	/**
+	 * @var
+	 */
+	public static $_microtime;
 
-  /**
-   * @var
-   */
-  public static $microtime;
+	/**
+	 * @var
+	 */
+	public static $microtime;
 
-  /**
-   * @var
-   */
-  public static $memory;
+	/**
+	 * @var
+	 */
+	public static $memory;
 
-  /**
-   * @var
-   */
-  public static $url;
+	/**
+	 * @var
+	 */
+	public static $url;
 
-  /**
-   * @var
-   */
-  public static $page;
+	/**
+	 * @var
+	 */
+	public static $page;
 
-  /**
-   * @var
-   */
-  public static $timezone;
+	/**
+	 * @var
+	 */
+	public static $timezone;
 
-  /**
-   * @var
-   */
-  public static $environment;
+	/**
+	 * @var
+	 */
+	public static $environment;
 
-  /**
-   * @var null[]
-   */
-  public static $namespaces = [
-    'controller' => null,
-    'middleware' => null,
-    'model' => null
-  ];
+	/**
+	 * @var null[]
+	 */
+	public static $namespaces = [
+		'controller' => null,
+		'middleware' => null,
+		'model' => null
+	];
 
-  /**
-   * @param array $data
-   * @return void
-   */
-  public static function init(array $data = []): void
-  {
-    if (self::$active) return;
+	/**
+	 * @param array $data
+	 *
+	 * @return void
+	 */
+	public static function init(array $data = []): void
+	{
+		if (self::$active)
+			return;
 
-    self::$_microtime = microtime(true);
+		self::$_microtime = microtime(true);
 
-	  self::$dotenv = Env::init();
-    self::init_Define();
-    self::init_Header();
-    //self::init_Session();
-    self::init_Whoops($data);
-    self::init_Data($data);
-    if (isset($_ENV['LOG']) && $_ENV['LOG'] == "true") self::init_Monolog();
-    if (isset($_ENV['I18N']) && $_ENV['I18N'] == "true") (new Locale());
-    self::_routes();
+		self::$dotenv = Env::init();
+		self::init_Define();
+		self::init_Header();
+		self::init_Session($data);
+		self::init_Whoops($data);
+		self::init_Data($data);
+		if (isset($_ENV['LOG']) && $_ENV['LOG'] == "true")
+			self::init_Monolog();
+		if (isset($_ENV['I18N']) && $_ENV['I18N'] == "true")
+			(new Locale());
+		self::_routes();
 
-    if (@$data['namespaces'] != null) self::$namespaces = $data['namespaces'];
+		if (@$data['namespaces'] != null)
+			self::$namespaces = $data['namespaces'];
 
-    if (@self::$namespaces['controller']) {
-      Controller::namespace(@self::$namespaces['controller']);
-    }
-    if (@self::$namespaces['middleware']) {
-      Middleware::namespace(@self::$namespaces['middleware']);
-    }
-    if (@self::$namespaces['model']) {
-      Model::namespace(@self::$namespaces['model']);
-    }
-    
-    self::init_Route();
+		if (@self::$namespaces['controller']) {
+			Controller::namespace(@self::$namespaces['controller']);
+		}
+		if (@self::$namespaces['middleware']) {
+			Middleware::namespace(@self::$namespaces['middleware']);
+		}
+		if (@self::$namespaces['model']) {
+			Model::namespace(@self::$namespaces['model']);
+		}
 
-    self::$active = true;
-  }
+		self::init_Route();
 
-  /**
-   * @param $par
-   * @param string|null $value
-   * @param bool $get
-   * @param string|null $sub
-   * @param bool $new
-   * @return array|App|null[]|string|void
-   */
-  public static function SGnamespace($par, string $value = null, bool $get = false, string $sub = null, bool $new = false)
-  {
-    $sub = ($sub != null) ? (CL::trim($sub) . '\\') : null;
+		self::$active = true;
+	}
 
-    if (is_string($par)) {
-      if (array_key_exists($par, self::$namespaces)) {
-        self::$namespaces[$par] = (CL::trim(($sub . $value)) . '\\');
-        if ($get === true) {
-          return self::$namespaces[$par];
-        }
-      }
-    } elseif (is_array($par)) {
-      foreach (@$par as $key) {
-        if (array_key_exists($key, self::$namespaces)) {
-          self::$namespaces[$key] = (CL::trim(($sub . $value)) . '\\');
-          if ($get === true) {
-            return self::$namespaces[$key];
-          }
-        }
-      }
+	/**
+	 * @param $par
+	 * @param string|null $value
+	 * @param bool $get
+	 * @param string|null $sub
+	 * @param bool $new
+	 *
+	 * @return array|App|null[]|string|void
+	 */
+	public static function SGnamespace($par, string $value = null, bool $get = false, string $sub = null, bool $new = false)
+	{
+		$sub = ($sub != null) ? (CL::trim($sub) . '\\') : null;
 
-      foreach (@$par as $key => $val) {
-        if (array_key_exists($key, self::$namespaces)) {
-          self::$namespaces[$key] = (CL::trim(($sub . $val)) . '\\');
-          if ($get === true) {
-            return self::$namespaces[$key];
-          }
-        }
-      }
-    } else {
-      if ($get === true) {
-        return self::$namespaces;
-      }
-    }
-    if ($new) return new self;
-  }
+		if (is_string($par)) {
+			if (array_key_exists($par, self::$namespaces)) {
+				self::$namespaces[$par] = (CL::trim(($sub . $value)) . '\\');
+				if ($get === true) {
+					return self::$namespaces[$par];
+				}
+			}
+		} else if (is_array($par)) {
+			foreach (@$par as $key) {
+				if (array_key_exists($key, self::$namespaces)) {
+					self::$namespaces[$key] = (CL::trim(($sub . $value)) . '\\');
+					if ($get === true) {
+						return self::$namespaces[$key];
+					}
+				}
+			}
 
-  /**
-   * @param array $namespaces
-   * @param string|null $sub
-   * @param bool $new
-   * @return App|void
-   */
-  public static function namespace(array $namespaces = [], string $sub = null, bool $new = false)
-  {
-    self::SGnamespace($namespaces, null, false, $sub);
-    if ($new) return new self;
-  }
+			foreach (@$par as $key => $val) {
+				if (array_key_exists($key, self::$namespaces)) {
+					self::$namespaces[$key] = (CL::trim(($sub . $val)) . '\\');
+					if ($get === true) {
+						return self::$namespaces[$key];
+					}
+				}
+			}
+		} else {
+			if ($get === true) {
+				return self::$namespaces;
+			}
+		}
+		if ($new)
+			return new self;
+	}
 
-  /**
-   * @param string $key
-   * @return void
-   */
-  public static function get(string $key)
-  {
-    if (in_array($key, get_class_vars(__CLASS__))) {
-      return self::${$key};
-    }
-  }
+	/**
+	 * @param array $namespaces
+	 * @param string|null $sub
+	 * @param bool $new
+	 *
+	 * @return App|void
+	 */
+	public static function namespace(array $namespaces = [], string $sub = null, bool $new = false)
+	{
+		self::SGnamespace($namespaces, null, false, $sub);
+		if ($new)
+			return new self;
+	}
 
-  /**
-   * @return void
-   */
-  private static function init_Define(): void
-  {
-    # URL
-    self::$url = Util::base_url();
-    @define('URL', self::$url);
+	/**
+	 * @param string $key
+	 *
+	 * @return void
+	 */
+	public static function get(string $key)
+	{
+		if (in_array($key, get_class_vars(__CLASS__))) {
+			return self::${$key};
+		}
+	}
 
-    # PAGE
-    self::$page = Util::page_url();
-    @define('PAGE', self::$page);
+	/**
+	 * @return void
+	 */
+	private static function init_Define(): void
+	{
+		# URL
+		self::$url = Util::base_url();
+		@define('URL', self::$url);
 
-    # TIMEZONE
-    self::$timezone = ((isset($_ENV['TIMEZONE']) && $_ENV['TIMEZONE'] != null) ? $_ENV['TIMEZONE'] : 'Europe/Istanbul');
-    @define('TIMEZONE', self::$timezone);
-    @date_default_timezone_set(self::$timezone);
+		# PAGE
+		self::$page = Util::page_url();
+		@define('PAGE', self::$page);
 
-    # ENVIRONMENT
-    self::$environment = ((isset($_ENV['ENVIRONMENT']) && $_ENV['ENVIRONMENT'] != null) ? $_ENV['ENVIRONMENT'] : 'development');
-    @define('ENVIRONMENT', self::$environment);
+		# TIMEZONE
+		self::$timezone = ((isset($_ENV['TIMEZONE']) && $_ENV['TIMEZONE'] != null) ? $_ENV['TIMEZONE'] : 'Europe/Istanbul');
+		@define('TIMEZONE', self::$timezone);
+		@date_default_timezone_set(self::$timezone);
 
-    switch (self::$environment) {
-      case 'staging':
-      case 'development':
-        @error_reporting(-1);
-        @ini_set('display_errors', '1');
-        @error_reporting(E_ALL ^ E_WARNING ^ E_USER_WARNING ^ E_NOTICE ^ E_DEPRECATED);
-        break;
-      case 'testing':
-      case 'production':
-        @ini_set('display_errors', '0');
-        if (version_compare(PHP_VERSION, '5.3', '>=')) {
-          @error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-        } else {
-          @error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-        }
-        break;
-      default:
-        @header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'The application environment is not set correctly.';
-        exit(1);
-    }
-  }
+		# ENVIRONMENT
+		self::$environment = ((isset($_ENV['ENVIRONMENT']) && $_ENV['ENVIRONMENT'] != null) ? $_ENV['ENVIRONMENT'] : 'development');
+		@define('ENVIRONMENT', self::$environment);
 
-  /**
-   * @return void
-   */
-  private static function init_Header(): void
-  {
-    @header_remove();
-    @header('Date: ' . date('D, d M Y H:i:s') . ' GMT');
-    @header('Strict-Transport-Security: max-age=15552000; preload');
-    @header('X-Frame-Options: sameorigin');
-    if (self::$environment == 'development') {
-      @header('X-Powered-By: PHP/BMVC');
-    }
-    if (self::$page) @header('X-Url: ' . self::$page);
-    @header('X-XSS-Protection: 1; mode=block');
-  }
+		switch (self::$environment) {
+			case 'staging':
+			case 'development':
+				@error_reporting(-1);
+				@ini_set('display_errors', '1');
+				@error_reporting(E_ALL ^ E_WARNING ^ E_USER_WARNING ^ E_NOTICE ^ E_DEPRECATED);
+				break;
+			case 'testing':
+			case 'production':
+				@ini_set('display_errors', '0');
+				if (version_compare(PHP_VERSION, '5.3', '>=')) {
+					@error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+				} else {
+					@error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+				}
+				break;
+			default:
+				@header('HTTP/1.1 503 Service Unavailable.', true, 503);
+				echo 'The application environment is not set correctly.';
+				exit(1);
+		}
+	}
 
-  /**
-   * @return void
-   */
-  public static function init_Session(): void
-  {
-    if (session_status() != PHP_SESSION_ACTIVE || session_id() == null) {
-      @ini_set('session.use_only_cookies', '1');
-      @session_set_cookie_params([
-        'lifetime' => 3600 * 24,
-        'httponly' => true,
-        // 'path' => self::$url
-      ]);
-      if (self::$environment == 'development') {
-        @session_name('BMVC');
-      }
-      @session_start();
-    }
-  }
+	/**
+	 * @return void
+	 */
+	private static function init_Header(): void
+	{
+		@header_remove();
+		@header('Date: ' . date('D, d M Y H:i:s') . ' GMT');
+		@header('Strict-Transport-Security: max-age=15552000; preload');
+		@header('X-Frame-Options: sameorigin');
+		if (self::$environment == 'development') {
+			@header('X-Powered-By: PHP/BMVC');
+		}
+		if (self::$page)
+			@header('X-Url: ' . self::$page);
+		@header('X-XSS-Protection: 1; mode=block');
+	}
 
-  /**
-   * @param array $data
-   * @return void
-   */
-  private static function init_Whoops(array $data = []): void
-  {
-    $blacklist = array_keys($_ENV);
+	/**
+	 * @return void
+	 */
+	public static function init_Session($data): void
+	{
+		if ($data['session'] == true) {
+			if (session_status() != PHP_SESSION_ACTIVE || session_id() == null) {
+				@ini_set('session.use_only_cookies', '1');
+				@session_set_cookie_params([
+					'lifetime' => 3600 * 24,
+					'httponly' => true,
+					// 'path' => self::$url
+				]);
+				if (self::$environment == 'development') {
+					@session_name('BMVC');
+				}
+				@session_start();
+			}
+		}
+	}
 
-    Whoops::blacklist('_SERVER', $blacklist);
-    Whoops::blacklist('_ENV', $blacklist);
+	/**
+	 * @param array $data
+	 *
+	 * @return void
+	 */
+	private static function init_Whoops(array $data = []): void
+	{
+		$blacklist = array_keys($_ENV);
 
-    # Config Black List
-    if (isset($data['whoops_blacklist'])) {
-      foreach ($data['whoops_blacklist'] as $key => $val) {
-        Whoops::blacklist($key, $val);
-      }
-    }
+		Whoops::blacklist('_SERVER', $blacklist);
+		Whoops::blacklist('_ENV', $blacklist);
 
-    Whoops::set('environment', self::$environment);
-    Whoops::init();
-  }
+		# Config Black List
+		if (isset($data['whoops_blacklist'])) {
+			foreach ($data['whoops_blacklist'] as $key => $val) {
+				Whoops::blacklist($key, $val);
+			}
+		}
 
-  /**
-   * @param array $data
-   * @return void
-   */
-  private static function init_Data(array $data = []): void
-  {
-    if ($data != null) {
-      # File Import
-      if (isset($data['files'])) {
-        foreach ($data['files'] as $file) {
-          require_once $file;
-        }
-      }
-      # Class Load
-      if (isset($data['init'])) {
-        foreach ($data['init'] as $init) {
-          if (is_callable($init)) {
-            $init();
-          } else {
-            new $init;
-          }
-        }
-      }
-    }
-    #
+		Whoops::set('environment', self::$environment);
+		Whoops::init();
+	}
 
-    if (function_exists('mb_internal_encoding')) @mb_internal_encoding('UTF-8');
+	/**
+	 * @param array $data
+	 *
+	 * @return void
+	 */
+	private static function init_Data(array $data = []): void
+	{
+		if ($data != null) {
+			# File Import
+			if (isset($data['files'])) {
+				foreach ($data['files'] as $file) {
+					require_once $file;
+				}
+			}
+			# Class Load
+			if (isset($data['init'])) {
+				foreach ($data['init'] as $init) {
+					if (is_callable($init)) {
+						$init();
+					} else {
+						new $init;
+					}
+				}
+			}
+		}
+		#
 
-    if (Util::is_cli()) die('Cli Not Available, Browser Only.');
-  }
+		if (function_exists('mb_internal_encoding'))
+			@mb_internal_encoding('UTF-8');
 
-  /**
-   * @return void
-   */
-  private static function init_Monolog(): void
-  {
-    Monolog::init();
+		if (Util::is_cli())
+			die('Cli Not Available, Browser Only.');
+	}
 
-    if (Monolog::$log) {
-      Whoops::$whoops->pushHandler(function ($exception) {
-        Monolog::$log->error($exception);
-      });
-    }
-  }
+	/**
+	 * @return void
+	 */
+	private static function init_Monolog(): void
+	{
+		Monolog::init();
 
-  /**
-   * @return void
-   */
-  private static function init_Route(): void
-  {
-    if (@$_ENV['PUBLIC_DIR'] && strpos((string)Request::server('REQUEST_URI'), @$_ENV['PUBLIC_DIR'])) {
-      redirect(strstr((string)Request::server('REQUEST_URI'), [@$_ENV['PUBLIC_DIR'] => '/']));
-    }
+		if (Monolog::$log) {
+			Whoops::$whoops->pushHandler(function ($exception) {
+				Monolog::$log->error($exception);
+			});
+		}
+	}
 
-    if (strstr((string)@Request::_server('REQUEST_URI'), ('/' . trim(@$_ENV['PUBLIC_DIR'], '/') . '/'))) Route::getErrors(404);
+	/**
+	 * @return void
+	 */
+	private static function init_Route(): void
+	{
+		if (@$_ENV['PUBLIC_DIR'] && strpos((string)Request::server('REQUEST_URI'), @$_ENV['PUBLIC_DIR'])) {
+			redirect(strstr((string)Request::server('REQUEST_URI'), [@$_ENV['PUBLIC_DIR'] => '/']));
+		}
 
-    $route = Route::Run();
+		if (strstr((string)@Request::_server('REQUEST_URI'), ('/' . trim(@$_ENV['PUBLIC_DIR'], '/') . '/')))
+			Route::getErrors(404);
 
-    # MICROTIME
-    self::$microtime = number_format(microtime(true) - self::$microtime, 3);
-    @define('MICROTIME', self::$microtime);
+		$route = Route::Run();
 
-    # MEMORY
-    self::$memory = round(memory_get_usage() / 1024, 4);
-    @define('MEMORY', self::$memory);
+		# MICROTIME
+		self::$microtime = number_format(microtime(true) - self::$microtime, 3);
+		@define('MICROTIME', self::$microtime);
 
-    if (@$route) {
+		# MEMORY
+		self::$memory = round(memory_get_usage() / 1024, 4);
+		@define('MEMORY', self::$memory);
 
-      if (@$route['namespaces'] != null && is_array($route['namespaces'])) {
-        foreach ($route['namespaces'] as $key => $val) {
-          if (array_key_exists($key, self::$namespaces)) {
-            call_user_func_array([CL::implode([__NAMESPACE__, ucfirst($key)]), 'namespace'], [$val]);
-          }
-        }
-      }
+		if (@$route) {
 
-      if (array_key_exists('middlewares', $route)) {
-        foreach ($route['middlewares'] as $key => $val) {
-          Middleware::call(@$val['callback']);
-        }
-      }
+			if (@$route['namespaces'] != null && is_array($route['namespaces'])) {
+				foreach ($route['namespaces'] as $key => $val) {
+					if (array_key_exists($key, self::$namespaces)) {
+						call_user_func_array([CL::implode([__NAMESPACE__, ucfirst($key)]), 'namespace'], [$val]);
+					}
+				}
+			}
 
-      Controller::call(@$route['action'], @$route['params']);
+			if (array_key_exists('middlewares', $route)) {
+				foreach ($route['middlewares'] as $key => $val) {
+					Middleware::call(@$val['callback']);
+				}
+			}
 
-      if (@$route['_return'] && !Header::check_type(@$route['_return'])) Route::getErrors(404);
-    } else {
-      Route::getErrors(404);
-    }
-  }
+			Controller::call(@$route['action'], @$route['params']);
 
-  /**
-   * @return void
-   */
-  private static function _routes(): void
-  {
-    Route::match(['GET', 'POST'], 'route/:all', function ($url) {
-      $_url = Route::url($url);
-      if ($_url) {
-        url($_url, (bool)!Request::get('return'));
-      } else {
-        Route::getErrors(404);
-      }
-    });
-  }
+			if (@$route['_return'] && !Header::check_type(@$route['_return']))
+				Route::getErrors(404);
+		} else {
+			Route::getErrors(404);
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	private static function _routes(): void
+	{
+		Route::match(['GET', 'POST'], 'route/:all', function ($url) {
+			$_url = Route::url($url);
+			if ($_url) {
+				url($_url, (bool)!Request::get('return'));
+			} else {
+				Route::getErrors(404);
+			}
+		});
+	}
 }
